e8a2c24e7dbf20e6abd4364daca238222873fa36
HoaLe <lvhoa58@gmail.com>
2021-08-01 13:15:29 +0700

    add crowdsale contract

--- README.md
+++ README.md
@@ -34,3 +34,7 @@ npx truffle test --network test
 ```
 truffle run verify KFIVE@0x2991be6411cd52243f160fb0e16a81a37279d944 --network mainnet
 ```
+
+# Refs
+- CrowdSale: https://docs.openzeppelin.com/contracts/2.x/crowdsales
+- ICO: https://ico.readthedocs.io/en/latest/
---
+++ contracts/ICO/KFIVECrowdsale.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+import "./Crowdsale.sol";
+import "./validation/CappedCrowdsale.sol";
+import "./validation/TimedCrowdsale.sol";
+
+contract KFIVECrowdsale is Crowdsale, CappedCrowdsale, TimedCrowdsale {
+
+    constructor(
+        uint256 rate,            // rate, in TKNbits
+        address payable wallet,  // wallet to send Ether
+        IBEP20 token,            // the token
+        uint256 cap,             // total cap, in wei
+        uint256 openingTime,     // opening time in unix epoch seconds
+        uint256 closingTime      // closing time in unix epoch seconds
+    )
+        CappedCrowdsale(cap)
+        TimedCrowdsale(openingTime, closingTime)
+        Crowdsale(rate, wallet, token)
+    {
+        // nice, we just created a crowdsale that's only open
+        // for a certain amount of time
+        // and stops accepting contributions once it reaches `cap`
+    }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view override (Crowdsale, CappedCrowdsale, TimedCrowdsale) {
+        super._preValidatePurchase(beneficiary, weiAmount);
+    }
+}
