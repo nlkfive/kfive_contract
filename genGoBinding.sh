313b9d8990d5b2e2eca40e4bde96c997a5a000cd
HoaLe <lvhoa58@gmail.com>
2022-04-15 10:07:28 +0700

    update package path

--- genGoBinding.sh
+++ genGoBinding.sh
@@ -6,6 +6,7 @@ contracts=$(find $base_dir/abi -mindepth 1 -maxdepth 1 -exec basename {} \;)
 for contract in $contracts; do
     echo $contract
     name=${contract%.*}
-    abigen --abi $base_dir/abi/$contract --pkg "$name" --out $base_dir/golang/${name}.go
+    mkdir -p $base_dir/golang/$name
+    abigen --abi $base_dir/abi/$contract --pkg "$name" --out $base_dir/golang/$name/${name}.go
 done
 
